9d8f47b34fa725b0abdd27a8d5deb0f0e7cd0887
ostanioni <ostanioni@gmail.com>
2021-04-10 17:25:58 +0700

    переместил

--- Krinkin/Linux_programming_basic/1_2/say_hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "say_hello.h"
-
-void say_hello(const char * name) {
-	printf("Hello %s \n", name);
-}
--- Krinkin/Linux_programming_basic/1_2/say_hello.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SAY_HELLO__H
-#define __SAY_HELLO__H
-
-void say_hello(const char* name);
-
-#endif
