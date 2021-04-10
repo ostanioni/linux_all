8b176882b4d5d15490be368009160a1ef9e9f7b5
ostanioni <ostanioni@gmail.com>
2021-04-10 17:38:25 +0700

    file was added

---
+++ Krinkin/Linux_programming_basic/1_2/test/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+int (*someSecretFunctionPrototype)(int);
+
+int main(int argc, char * argv[])
+{
+	void *hdl = dlopen(argv[1], RTLD_LAZY);
+	someSecretFunctionPrototype = (void (*)(const char*))dlsym(hdl, "someSecretFunctionPrototype");
+	
+	int result = someSecretFunctionPrototype(argv[2]);
+	printf("\n%d", result);
+	
+	dlclose(hdl);
+	return 0;
+}
